ab05d248f3cb81b45d26e85bdddf65051683c4e7
Pels <pelumisunday@gmail.com>
2022-10-09 19:06:12 +0100

    cleanup: remove unused boilerplate files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Hello World</h1>
 </template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
