7feb900255a9ab2575a3fd962e6862c945b77d31
Pels <pelumisunday@gmail.com>
2022-10-10 10:30:09 +0100

    feat: create text area component

---
+++ src/components/TextAreaInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="w-full max-w-md border rounded overflow-hidden shadow bg-white-50"
+  >
+    <label
+      for="texts"
+      class="text-sm text-gray-600 font-medium inline-block px-2 py-3 italic"
+      >Vue Virtual Keyboard</label
+    >
+    <textarea
+      id="texts"
+      class="shadow form-textarea block w-full py-2 px-3 resize-none text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-gray-300"
+      rows="5"
+      placeholder="Hey..."
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextAreaInput",
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Hello World</h1>
+  <main class="min-h-screen p-8 bg-gray-50 flex justify-center items-center">
+    <TextAreaInput />
+  </main>
 </template>
 
 <script>
+import TextAreaInput from "@/components/TextAreaInput.vue";
 export default {
   name: "HomeView",
+  components: { TextAreaInput },
 };
 </script>
