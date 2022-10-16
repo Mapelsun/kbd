580ab3aeeab9100054b3ca3ce6483327527f51f1
Pels <pelumisunday@gmail.com>
2022-10-16 19:21:04 +0100

    feat: lowercase and uppercase toggling

--- src/components/KeyboardInput.vue
+++ src/components/KeyboardInput.vue
@@ -4,10 +4,28 @@
       class="w-full max-w-2xl rounded bg-gray-500 text-gray-800 text-xs flex items-center justify-center"
     >
       <div class="p-1 overflow-hidden w-full">
-        <KeyboardRow class="mb-[3px]" :keys="getRowData('1')" />
-        <KeyboardRow class="mb-[3px]" :keys="getRowData('2')" />
-        <KeyboardRow class="mb-[3px]" :keys="getRowData('3')" />
-        <KeyboardRow class="mb-[3px]" :keys="getRowData('4')" />
+        <KeyboardRow
+          class="mb-[3px]"
+          :keys="getRowData('1')"
+          :switchBtnKey="switchBtnKey"
+        />
+        <KeyboardRow
+          class="mb-[3px]"
+          :keys="getRowData('2')"
+          :switchBtnKey="switchBtnKey"
+        />
+        <KeyboardRow
+          class="mb-[3px]"
+          :keys="getRowData('3')"
+          @toggleBtnKey="toggleBtnKey"
+          :switchBtnKey="switchBtnKey"
+        />
+        <KeyboardRow
+          class="mb-[3px]"
+          :keys="getRowData('4')"
+          @toggleBtnKey="toggleBtnKey"
+          :switchBtnKey="switchBtnKey"
+        />
         <KeyboardRow :keys="getRowData('5')" />
       </div>
     </main>
@@ -21,6 +39,7 @@ export default {
   components: { KeyboardRow },
   data() {
     return {
+      switchBtnKey: true,
       keys: [
         { label: ["~", "`"], width: "w-10", row: "1" },
         { label: ["!", "1"], width: "w-10", row: "1" },
@@ -37,40 +56,40 @@ export default {
         { label: ["+", "="], width: "w-10", row: "1" },
         { label: "backspace", width: "w-full", row: "1", flexGrow: true },
         { label: "tab", width: "w-14", row: "2" },
-        { label: "Q", width: "w-10", row: "2" },
-        { label: "W", width: "w-10", row: "2" },
-        { label: "E", width: "w-10", row: "2" },
-        { label: "R", width: "w-10", row: "2" },
-        { label: "T", width: "w-10", row: "2" },
-        { label: "Y", width: "w-10", row: "2" },
-        { label: "U", width: "w-10", row: "2" },
-        { label: "I", width: "w-10", row: "2" },
-        { label: "O", width: "w-10", row: "2" },
-        { label: "P", width: "w-10", row: "2" },
+        { label: ["q", "Q"], width: "w-10", row: "2" },
+        { label: ["w", "W"], width: "w-10", row: "2" },
+        { label: ["e", "E"], width: "w-10", row: "2" },
+        { label: ["r", "R"], width: "w-10", row: "2" },
+        { label: ["t", "T"], width: "w-10", row: "2" },
+        { label: ["y", "Y"], width: "w-10", row: "2" },
+        { label: ["u", "U"], width: "w-10", row: "2" },
+        { label: ["i", "I"], width: "w-10", row: "2" },
+        { label: ["o", "O"], width: "w-10", row: "2" },
+        { label: ["p", "P"], width: "w-10", row: "2" },
         { label: "[", width: "w-12", row: "2" },
         { label: "]", width: "w-12", row: "2" },
         { label: "|", width: "w-full", row: "2", flexGrow: true },
         { label: "caps", width: "w-16", row: "3" },
-        { label: "A", width: "w-10", row: "3" },
-        { label: "S", width: "w-10", row: "3" },
-        { label: "D", width: "w-10", row: "3" },
-        { label: "F", width: "w-10", row: "3" },
-        { label: "G", width: "w-10", row: "3" },
-        { label: "H", width: "w-10", row: "3" },
-        { label: "J", width: "w-10", row: "3" },
-        { label: "K", width: "w-10", row: "3" },
-        { label: "L", width: "w-10", row: "3" },
+        { label: ["a", "A"], width: "w-10", row: "3" },
+        { label: ["s", "S"], width: "w-10", row: "3" },
+        { label: ["d", "D"], width: "w-10", row: "3" },
+        { label: ["f", "F"], width: "w-10", row: "3" },
+        { label: ["g", "G"], width: "w-10", row: "3" },
+        { label: ["h", "H"], width: "w-10", row: "3" },
+        { label: ["j", "J"], width: "w-10", row: "3" },
+        { label: ["j", "K"], width: "w-10", row: "3" },
+        { label: ["l", "L"], width: "w-10", row: "3" },
         { label: ";", width: "w-10", row: "3" },
         { label: "'", width: "w-10", row: "3" },
         { label: "enter", width: "w-full", row: "3", flexGrow: true },
         { label: "shift", width: "w-20", row: "4" },
-        { label: "Z", width: "w-10", row: "4" },
-        { label: "X", width: "w-10", row: "4" },
-        { label: "C", width: "w-10", row: "4" },
-        { label: "V", width: "w-10", row: "4" },
-        { label: "B", width: "w-10", row: "4" },
-        { label: "N", width: "w-10", row: "4" },
-        { label: "M", width: "w-10", row: "4" },
+        { label: ["z", "Z"], width: "w-10", row: "4" },
+        { label: ["x", "X"], width: "w-10", row: "4" },
+        { label: ["c", "C"], width: "w-10", row: "4" },
+        { label: ["v", "V"], width: "w-10", row: "4" },
+        { label: ["b", "B"], width: "w-10", row: "4" },
+        { label: ["n", "N"], width: "w-10", row: "4" },
+        { label: ["m", "M"], width: "w-10", row: "4" },
         { label: ",", width: "w-10", row: "4" },
         { label: ".", width: "w-10", row: "4" },
         { label: "/", width: "w-10", row: "4" },
@@ -93,6 +112,9 @@ export default {
   methods: {
     getRowData(rowId) {
       return this.keys.filter((key) => key.row === rowId);
+    },
+    toggleBtnKey() {
+      this.switchBtnKey = !this.switchBtnKey;
     },
   },
 };
